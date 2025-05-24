8aa4267a67b8ab3477bf73869255eef4669a1ba0
ParasRana123 <parasrana579@gmail.com>
2025-05-24 15:35:46 +0530

    Created room in which user name displayed

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
 export function Landing() {
-    return <div>Landing Page</div>;
+    const [name , setName] = useState("")
+    return <div>
+        <input type="text" onChange={(e) => {
+            setName(e.target.value);
+        }}>
+        </input>
+        <Link to={`/room/?name=${name}`}>Join</Link>
+    </div>;
 }
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom"
+
 export function Room() {
-    return <div>Room</div>
+    const [searchParams , setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+
+    useEffect(() => {
+        // logic to init the user to the room
+        
+    } , [name])
+
+    return <div>
+        Hi {name}
+    </div>
 }
