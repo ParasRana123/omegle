db00d1ade9706ecd1f48fcea935b1758d2750a46
ParasRana123 <parasrana579@gmail.com>
2025-05-25 12:05:17 +0530

    wrote some frontend logic and fixed backend logic

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,13 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
+import { io, Socket } from "socket.io-client"
+
+const URL = "http://localhost:3000"
 
 export function Room() {
     const [searchParams , setSearchParams] = useSearchParams();
     const name = searchParams.get('name');
+    const [socket , setSocket] = useState<null | Socket>(null);
 
     useEffect(() => {
         // logic to init the user to the room
-        
+        const socket = io(URL , {
+            autoConnect: false
+        });
+        setSocket(socket);
+
+        socket.on("send-offer" , ({roomId}) => {
+            alert("Send offer please");
+            socket.emit("offer" , {
+                sdp: "",
+                roomId
+            })
+        })
+        socket.on("offer" , ({roomId , offer}) => {
+            alert("Send answer please");
+            socket.emit("answer" , {
+                sdp: "",
+                roomId
+            })
+        })
+        socket.on("answer" , ({roomId , answer}) => {
+            alert("Connection done");
+        })
+
+
     } , [name])
 
     return <div>
