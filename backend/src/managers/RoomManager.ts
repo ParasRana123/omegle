e6e5c65a0a6a92c8689213cea67b92aca8b9255d
ParasRana123 <parasrana579@gmail.com>
2025-05-29 17:32:37 +0530

    Fixed some backend code for room video rendering:

--- backend/src/managers/RoomManager.ts
+++ backend/src/managers/RoomManager.ts
@@ -1,64 +1,70 @@
-import { User } from "./UserManager";
+ import { User } from "./UserManager";
 
 let GLOBAL_ROOM_ID = 1;
 
-export interface Room {
+interface Room {
     user1: User,
     user2: User,
 }
 
 export class RoomManager {
-    private rooms: Map<string , Room>
+    private rooms: Map<string, Room>
     constructor() {
-        this.rooms = new Map<string , Room>();
+        this.rooms = new Map<string, Room>()
     }
 
-    createRoom(user1: User , user2: User) {
+    createRoom(user1: User, user2: User) {
         const roomId = this.generate().toString();
-        this.rooms.set(roomId.toString() , {
-            user1,
+        this.rooms.set(roomId.toString(), {
+            user1, 
             user2,
         })
 
-        user1.socket.emit("send-offer" , {
+        user1.socket.emit("send-offer", {
+            roomId
+        })
+
+        user2.socket.emit("send-offer", {
             roomId
         })
     }
 
-    onOffer(roomId: string , sdp: string , senderSocketId: string) {
+    onOffer(roomId: string, sdp: string, senderSocketid: string) {
         const room = this.rooms.get(roomId);
-        if(!room) {
+        if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user2;
-        receivingUser?.socket.emit("offer" , {
+        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2: room.user1;
+        receivingUser?.socket.emit("offer", {
             sdp,
             roomId
         })
     }
-
-    onAnswer(roomId: string , sdp: string , socketId: string , senderSocketId: string) {
+    
+    onAnswer(roomId: string, sdp: string, senderSocketid: string) {
         const room = this.rooms.get(roomId);
-        if(!room) {
+        if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user2;
-        receivingUser?.socket.emit("answer" , {
+        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2: room.user1;
+
+        receivingUser?.socket.emit("answer", {
             sdp,
             roomId
-        })
+        });
     }
 
-    onIceCandidates(roomId: string , senderSocketId: string , candidate: any , type: "sender" | "reciever") {
+    onIceCandidates(roomId: string, senderSocketid: string, candidate: any, type: "sender" | "receiver") {
         const room = this.rooms.get(roomId);
-        if(!room) {
+        if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user2;
-        receivingUser.socket.emit("add-ice-candidate" , ({candidate , type}));
+        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2: room.user1;
+        receivingUser.socket.emit("add-ice-candidate", ({candidate, type}));
     }
 
     generate() {
         return GLOBAL_ROOM_ID++;
     }
+
 }
